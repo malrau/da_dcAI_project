fbc0d87ffacc0a7e954a42035285ccc37e18c37f
malrau <maurizio.larosa@unime.it>
2023-06-27 19:13:33 +0200

    Improve the model definition and code explanation

--- d_sequentialModel.py
+++ d_sequentialModel.py
@@ -8,25 +8,28 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import InputLayer, Dense, Activation, Flatten, Conv2D
 
 def seqModel(trainData, trainLabels, testData, testLabels):
-    # Build/Define the model
+    # Model definition: input and convolutional layers
     model = Sequential()
     model.add(InputLayer(input_shape = (56, 56, 3), name = 'Input')) # input layer
-    model.add(Conv2D(filters = 32, kernel_size = 3)) # 
-    model.add(Activation('ReLU', name = 'ReLU'))
-    model.add(Flatten())
+    model.add(Conv2D(filters = 32, kernel_size = 3)) # 2D convolution layer
+    model.add(Activation('ReLU', name = 'ReLU'))     # activation function
 
-    # Classify the image
+    # Model definition: image classification layers
+    model.add(Flatten())
     model.add(Dense(515, name = 'FC2'))
     model.add(Activation('softmax', name = 'Softmax'))
 
-    model.summary() #plot the model
+    # Print a summary of the layers' outputs and model parameters
+    model.summary()
 
     # Compile and train the model (choose loss function, gradient descent...)
     model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
+    # Assign accuracy and loss values to history object
     history = model.fit(trainData, trainLabels, epochs = 10, batch_size = 64, validation_split = 0.20)
 
+    # Evaluate the model
     test_loss, test_acc = model.evaluate(testData, testLabels, verbose=0)
-    print('\nTest accuracy:', test_acc)
+    print('\nTest loss:', test_loss, '\nTest accuracy:', test_acc)
     
     return history
