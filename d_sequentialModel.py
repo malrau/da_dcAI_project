9abe66840acd42ae72d92d9a11411289414433b6
malrau <maurizio.larosa@unime.it>
2023-06-29 08:20:02 +0200

    Edit sequential model (v2)

--- d_sequentialModel.py
+++ d_sequentialModel.py
@@ -5,14 +5,21 @@
 
 ### IMPORT REQUIRED MODULES ###
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import InputLayer, Dense, Activation, Flatten, Conv2D
+from tensorflow.keras.layers import InputLayer, Conv2D, MaxPooling2D, Activation, Flatten, Dense 
 
 def seqModel(trainData, trainLabels, testData, testLabels, batch_size, validation_generator, num_of_test_samples):
     # Model definition: input and convolutional layers
     model = Sequential()
     model.add(InputLayer(input_shape = (56, 56, 3), name = 'Input')) # input layer
-    model.add(Conv2D(filters = 32, kernel_size = 3)) # 2D convolution layer
+    model.add(Conv2D(filters = 16, kernel_size = 3)) # first 2D convolution layer
+    model.add(MaxPooling2D(3))
     model.add(Activation('ReLU', name = 'ReLU'))     # activation function
+    model.add(Conv2D(filters = 32, kernel_size = 3)) # second 2D convolution layer
+    model.add(Activation('ReLU', name = 'ReLU2'))    # activation function
+    model.add(MaxPooling2D(3))
+    model.add(Conv2D(filters = 64, kernel_size = 3)) # third 2D convolution layer
+    model.add(Activation('ReLU', name = 'ReLU3'))    # activation function
+    model.add(MaxPooling2D(3))
 
     # Model definition: image classification layers
     model.add(Flatten())
@@ -32,7 +39,7 @@ def seqModel(trainData, trainLabels, testData, testLabels, batch_size, validatio
     test_loss, test_acc = model.evaluate(testData, testLabels, verbose=0)
     print('\nTest loss:', test_loss, '\nTest accuracy:', test_acc)
 
-    # Perform prediction
+    # Predict
     prediction = model.predict(validation_generator, num_of_test_samples // (batch_size + 1))
 
     return history, prediction
